3ca66fb7e2b6715cb7794f1c56a7e7f6cee58224
Sonam Ngawang <s.ngawang@gmail.com>
2018-02-26 18:08:29 -0800

    adding little things

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 if __name__ == '__main__':
         socketio.run(app)
